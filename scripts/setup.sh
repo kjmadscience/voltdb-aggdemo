d4579c21fe4861d2922966b7f366f9993e11f224
David Rolfe <drolfe@voltdb.com>
2021-02-22 10:57:21 +0000

    bug fix

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 
-sqlcmd --servers=vdb1 < ../ddl/Ivoltdb-aggdemo-createDB.sql
+sqlcmd --servers=vdb1 < ../ddl/voltdb-aggdemo-createDB.sql
 java -jar $HOME/bin/addtodeploymentdotxml.jar vdb1,vdb2,vdb3 deployment topics.xml
